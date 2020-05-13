16d3b645fbb6bb81b2df481250e48af7d3e3003a
jusabatier <julien.sabatier@lepuyenvelay.fr>
2020-05-13 10:48:43 +0200

    Add IGN background

--- src/main/webapp/resources/scripts/configuration/config.js
+++ src/main/webapp/resources/scripts/configuration/config.js
@@ -13,8 +13,14 @@ config.backgrounds = [
 	{
 		url: 'https://opendata.agglo-lepuyenvelay.fr/geowebcache/service/wmts',
 		layer: 'rasters:ortho_2017',
-		title: 'Ortho 2017 - RTGE',
+		title: 'RTGE',
 		attribution: 'CRAIG, CAPeV, SDEHL, SGEV, SAEV, Enedis, FEDER'
+	},
+	{
+		url: 'https://opendata.agglo-lepuyenvelay.fr/geowebcache/service/wmts',
+		layer: 'ign:orthohr',
+		title: 'Photographies aériennes',
+		attribution: 'IGN'
 	},
 	{
 		url: 'https://opendata.agglo-lepuyenvelay.fr/geowebcache/service/wmts',
